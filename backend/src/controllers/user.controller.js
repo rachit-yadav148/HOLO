f0b852b71cef01a209978d53c6ddaeae8fb59d93
Rachit Yadav <yadavrachit148@gmail.com>
2025-06-30 16:52:42 +0530

    adding authentication page

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -14,11 +14,14 @@ try {
     if (!user) {
         return res.status(httpStatus.NOT_FOUND).json({message: "User Not Found"})
     }
-    if (bcrypt.compare(password, user.password)) {
+    let isPasswordCorrect = await bcrypt.compare(password, user.password)
+    if (isPasswordCorrect) {
         let token = crypto.randomBytes(20).toString("hex");
         user.token = token;
         await user.save();
         return res.status(httpStatus.OK).json({token: token})
+    } else {
+        return res.status(httpStatus.UNAUTHORIZED).json({message: "Invalid username or password"})
     }
 }
 catch (e) {
